f4321508960bd3ee873e0ae0a0d8f13e0ae8cc0e
WHAOOOOO <49232295+WHAOOOOO@users.noreply.github.com>
2020-09-10 20:57:30 +0800

    搜索插入位置

---
+++ easy/searchInsert.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            if nums[0] > target:
+                return 0
+            elif nums[-1] <= target:
+                return len(nums)
+            else:
+                for i in range(len(nums)):
+                    if (nums[i] <= target and target < nums[i + 1]) or nums[i + 1] is None:
+                        return i + 1
