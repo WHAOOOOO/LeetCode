cefa12574b8150d8e18399c60a533311d26f4f72
WHAOOOOO <49232295+WHAOOOOO@users.noreply.github.com>
2020-09-10 20:29:17 +0800

    移除元素

--- easy/removeElement.py
+++ easy/removeElement.py
@@ -1,5 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        '''
+        1. 先计算去掉关键值之后的数组长度
+        2. 使用while循环，如果是关键值则移到后边，计数器不变；如果不是关键值，计数器+1
+        '''
         count = 0
         a = 0
         for i in range(len(nums)):
