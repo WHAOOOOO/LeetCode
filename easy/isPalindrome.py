31ab6656910c0707fb6d8a0b902bec647d2f3454
WHAOOOOO <49232295+WHAOOOOO@users.noreply.github.com>
2020-09-10 15:31:37 +0800

    回文数

---
+++ easy/isPalindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        elif x >= 0 and x < 10:
+            return True
+        else:
+            a = 0
+            for i in range(len(str(x))):
+                if int(str(x)[i]) == int(str(x)[len(str(x)) - 1 - i]):
+                    continue
+                else:
+                    a = 1
+            if a == 0:
+                return True
+            else:
+                return False
