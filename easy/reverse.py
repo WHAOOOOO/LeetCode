cb18f440585e941e4e186878cbf1ce76f05be067
WHAOOOOO <49232295+WHAOOOOO@users.noreply.github.com>
2020-09-10 15:32:31 +0800

    整数反转

---
+++ easy/reverse.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        a = -x
+        b = ''
+        c = ''
+        if x == 0 or x < (-(2**31)) or x > ((2**31) - 1):
+            return 0
+        elif x > 0:
+            for i in str(x):
+                b = i + b
+            if int(b) < (-(2**31)) or int(b) > ((2**31) - 1):
+                return 0
+            else:
+                return int(b)
+        elif x < 0:
+            for i in str(a):
+                c = i + c
+            if -int(c) < (-(2**31)) or -int(c) > ((2**31) - 1):
+                return 0
+            else:
+                return -int(c)
