e238c64215aaede349014a9966401d1ecc27f6a6
WHAOOOOO <49232295+WHAOOOOO@users.noreply.github.com>
2020-09-10 10:27:07 +0800

    Create isValid.py

---
+++ isValid.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = {'(': ')', '[': ']', '{': '}'}
+        iList = []
+        if len(s) == 0 or len(s) / 2 != len(s) // 2:
+            return False
+        else:
+            for i in s:
+                if i in d:
+                    iList.append(i)
+                else:
+                    if iList == [] or d[iList.pop()] != i:
+                        return False
+        return iList == []
