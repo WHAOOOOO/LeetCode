4dc3fd783fc340699139f0e1d92d8059dd731bc2
wanghao <wanghaobjyd@163.com>
2020-09-18 15:37:28 +0800

    有效的括号

--- easy_0_100/isValid.py
+++ easy_0_100/isValid.py
@@ -1,5 +1,6 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+	# 有效的括号
         d = {'(': ')', '[': ']', '{': '}'}
         iList = []
         if len(s) == 0 or len(s) / 2 != len(s) // 2:
