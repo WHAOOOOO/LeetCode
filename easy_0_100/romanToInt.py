ab07fd74cecf5cbc004a17344518c62d40ea7d41
wanghao <wanghaobjyd@163.com>
2020-09-18 15:45:56 +0800

    罗马数字转整数

--- easy_0_100/romanToInt.py
+++ easy_0_100/romanToInt.py
@@ -1,5 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+	# 罗马数字转整数
         '''直接当前字母与下一个字母判断，只要小于下一个字母则为负'''
         rome = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         sum = 0
